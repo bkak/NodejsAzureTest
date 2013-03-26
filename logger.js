4b94174f782c95a4b07a032729ed42fdbb088e2a
Bhoomi <nice2seeyou@gmail.com>
2013-03-26 15:22:12 +0530

    ll

--- logger.js
+++ logger.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 module.exports = {
     write: function(text){
-            var stream = fs.createWriteStream("my_file.txt");
+            var stream = fs.createWriteStream("my_file.txt", {'flags': 'a'});
             stream.once('open', function(fd) {
                 stream.write(text + "\n");
                 stream.end();
